9f66a6a3aafd7549935a6f80086fe8ba443a8ca1
komachi12 <tk@hgbeyer.com>
2021-12-06 22:20:05 +0100

    Added prototype

---
+++ Domasna 2/Prototype/TravelMK/TravelMK/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using TravelMK.Models;
+
+namespace TravelMK.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Favorites()
+        {
+            return View();
+        }
+
+        public IActionResult Navigation()
+        {
+            return View();
+        }
+
+        public IActionResult Placeholder()
+        {
+            return View();
+        }
+    }
+}
---
+++ Domasna 2/Prototype/TravelMK/TravelMK/Models/Hotel.cs
@@ -0,0 +1,18 @@
+﻿namespace TravelMK.Models
+{
+    public class Hotel
+    {
+        public int ID { get; set; }
+        public double Lat { get; set; }
+        public double Lon { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public int HouseNumber { get; set; }
+        public int stars { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Website { get; set; }
+
+        public Hotel() { }
+    }
+}
